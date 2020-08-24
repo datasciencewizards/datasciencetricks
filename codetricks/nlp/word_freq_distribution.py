2a6409d61eb398b520c26e8788318e5a2f741471
datasciencewizards <60881058+datasciencewizards@users.noreply.github.com>
2020-08-24 16:30:45 +0530

    Update word_freq_distribution.py
    added wikipedia library to avoid HTML noise while parsing

--- codetricks/nlp/word_freq_distribution.py
+++ codetricks/nlp/word_freq_distribution.py
@@ -1,22 +1,19 @@
-from bs4 import BeautifulSoup
-import requests
 import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import wikipedia
 
-#fetching text from wikipedia
-res = requests.get('https://en.wikipedia.org/wiki/The_Big_Bang_Theory')
-soup = BeautifulSoup(res.text,"html5lib")
-text = soup.get_text(strip=True)
+#fetching text
+text = wikipedia.page("The Big Bang Theory").content
 
 #removing noise from data - stopwords and punctuations
 tokens = word_tokenize(text)
 stops = stopwords.words('english')
-punc = list(string.punctuation) + ["''", "``", "'s","-"] 
+punc = list(string.punctuation) + ["''", "``", "'s","-", "==", '==='] 
 noise = stops+ punc
 tokens = [w.lower() for w in tokens if not w.lower() in noise] 
 
 #frequency distribution graph
 freq = nltk.FreqDist(tokens)
-freq.plot(7)
+plt = freq.plot(7)
